cc7b1973fa845734ef70cf5a1f0d40503559c6af
maschaalbu <mariya.albu@n26.com>
2020-07-17 20:31:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/country_comparison")
-def hello():
+def country_comparison():
     return render_template('country_comparison.html')
 
 if __name__ == "__main__":
