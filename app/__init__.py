af322613b18462802fa7a1a9885a703eb0b7dfe0
Lars <larlan102@gmail.com>
2020-07-19 20:35:56 +0200

    possible scalingo fix

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 app.static_folder = 'static'
 
+
+from app import routes
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
-
-from app import routes
