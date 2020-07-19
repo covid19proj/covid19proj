42195774edcff3c84002c77e9119e1ce8c6910f4
maschaalbu <mariya.albu@n26.com>
2020-07-19 21:14:07 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,5 @@ app.static_folder = 'static'
 
 from app import routes
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
