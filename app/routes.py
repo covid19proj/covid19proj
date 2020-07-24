703baec6d93934a57df0388894dfe0f358eb8177
maschaalbu <mariya.albu@n26.com>
2020-07-24 23:07:17 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -6,16 +6,10 @@ import psycopg2
 pg_conn = psycopg2.connect("dbname='qnswfixs' user='qnswfixs' host='kandula.db.elephantsql.com' password='A82HCdQ9xDVrhHyDxxE_BHBGnx_WIeKB'")
 pg_cur = pg_conn.cursor()
 
-chart_type = "cases"
-
-selected_type = request.args.get("type")
-if selected_type:
-	chart_type = selected_type
-
 country_comparison_query = """
 select
 string_agg('{"name":"' || countries_and_territories || '",data:[' || (
-select string_agg(""" + chart_type + """_total::text,',') from
+select string_agg({0}_total::text,',') from
 (select * from reports_cumulative
 where countries_and_territories = c.countries_and_territories
 and date_rep >= c.first_case
@@ -27,10 +21,10 @@ countries c
 where countries_and_territories in (
 	select countries_and_territories from
 	(
-select distinct  on (countries_and_territories) countries_and_territories, """ + chart_type + """_total
+select distinct  on (countries_and_territories) countries_and_territories, {0}_total
 from reports_cumulative
-order by countries_and_territories, """ + chart_type + """_total desc
-) x order by """ + chart_type + """_total desc
+order by countries_and_territories, {0}_total desc
+) x order by {0}_total desc
 	limit 10
 )
 """
@@ -39,7 +33,7 @@ bubbles_query = """
 select
 string_agg('[' || m1.value::text || ',' || m2.value::text || ',' ||
 (
- SELECT """ + chart_type + """_total FROM reports_cumulative r
+ SELECT {0}_total FROM reports_cumulative r
  WHERE r.countries_and_territories = m1.countries_and_territories
  ORDER BY date_rep DESC
  LIMIT 1
@@ -52,10 +46,10 @@ m2.metric = 'median_age' and
 m1.countries_and_territories in (
 select countries_and_territories from
 (
-select distinct on (countries_and_territories) countries_and_territories, """ + chart_type + """_total
+select distinct on (countries_and_territories) countries_and_territories, {0}_total
 from reports_cumulative
-order by countries_and_territories, """ + chart_type + """_total desc
-) x order by """ + chart_type + """_total desc
+order by countries_and_territories, {0}_total desc
+) x order by {0}_total desc
 limit 25
 )
 """
@@ -65,7 +59,7 @@ select string_agg('{name:''' || continent_exp || ''',data:[' ||
 (
 select string_agg('{name:''' || countries_and_territories || ''',value:' ||
 	(
-		select """ + chart_type + """_total::text
+		select {0}_total::text
 		from reports_cumulative r
 		where r.countries_and_territories = c.countries_and_territories
 		order by date_rep desc
@@ -83,14 +77,19 @@ from continents con
 @app.route('/')
 @app.route('/index')
 def index():
-     # return render_template('index.html')
-    pg_cur.execute(country_comparison_query)
+    chart_type = "cases"
+
+    selected_type = request.args.get("type")
+    if selected_type:
+        chart_type = selected_type
+
+    pg_cur.execute(country_comparison_query.format(chart_type))
     row1 = pg_cur.fetchone()
 
-    pg_cur.execute(bubbles_query)
+    pg_cur.execute(bubbles_query.format(chart_type))
     row2 = pg_cur.fetchone()
 
-    pg_cur.execute(continents_query)
+    pg_cur.execute(continents_query.format(chart_type))
     row3 = pg_cur.fetchone()
 
     return render_template("index.html", country=row1[0], bubble=row2[0], continent=row3[0])
